84192793beec4c6643cbab78fe46998b47e7c1fc
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2021-12-23 12:24:53 +0530

    Comments get & post request handler added

--- comments/index.js
+++ comments/index.js
@@ -11,12 +11,33 @@ const commentsPostById = {};
 
 // Get comments by post id
 app.get('/posts/:id/comments', (req, res) => {
-
+    res.send(commentsPostById[req.params.id] || []);
 });
 
 // Post a comment to the particular post using ID 
 app.post('/posts/:id/comments', (req, res) => {
 
+    // Generate random ID for comment
+    let commentId = randomBytes(4).toString('hex');
+    
+    // Grab content from the request body
+    let { content } = req.body;
+
+    /**
+     * If comment already exists then fetch that array to push new comments
+     * or define blank array
+     */
+    let comments = commentsPostById[req.params.id] || [];
+
+    // Push new comment in the comments object to attach in particular post
+    comments.push({
+        id: commentId,
+        content
+    });
+
+    commentsPostById[req.params.id] = comments;
+
+    res.status(201).send(comments);
 });
 
 // Listen service on port
