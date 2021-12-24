c3572d1adfb83382732dbdcd223fd963827b86cf
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2021-12-24 10:55:16 +0530

    Posts list component has added in frontend

---
+++ client/src/PostsList.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default () => {
+    const [posts, setPosts] = useState({});
+
+    // Get & set posts data from posts api
+    const fetchPosts = async () => {
+        let res = await axios.get('http://localhost:4000/posts');
+
+        setPosts(res.data);
+    };
+
+    // Call the fetchPosts function only when componenet created for only once
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
+    // Render posts with array map
+    const renderedPosts = Object.values(posts).map( (post) => {
+        return <div className="card" key={post.id} style={{width: '33.33%', marginBottom: '20px'}}>
+            <div className="card-body">
+                <h3>{post.title}</h3>
+            </div>
+        </div>
+    });
+
+    return <div className="d-flex flex-row flex-wrap justify-content-between">
+        {renderedPosts}
+    </div>;
+};
