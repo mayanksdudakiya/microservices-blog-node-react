ca31173745f4c2e9b4e6b27a807d1bc57520790b
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2021-12-24 09:40:54 +0530

    Post create service logic has added

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from 'axios';
+
+
+
 
 export default () => {
+    const [title, setTitle] = useState('');
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        
+        await axios.post('http://localhost:4000/posts', {
+            title
+        });
+
+        setTitle('');
+    };
+
     return <div>
         <h1>Post Create</h1>
-        <form>
+        <form onSubmit={onSubmit}>
             <div className="form-group mb-2">
                 <label>Title</label>
-                <input type="text" className="form-control"/>
+                <input 
+                    type="text" 
+                    value={title} 
+                    onChange={e => setTitle(e.target.value)} 
+                    className="form-control"
+                />
             </div>
             <button className="btn btn-primary">Submit</button>
         </form>
